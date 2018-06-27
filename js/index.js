509b1ad21031a0161958a3db97e4042f1665d9f4
thirugiri <thirugiritharan@gmail.com>
2018-06-27 15:24:18 +0530

    deleted update

--- js/index.js
+++ js/index.js
@@ -2,5 +2,23 @@
  $(document).ready(function(){
     $('.sidenav').sidenav();
  });
+
+ $(document).ready(function(){
+    $('.carousel').carousel();
+
+  });
+      
+ $('.carousel').carousel({
+    padding: 200    
+});
+setTimeout(autoplay, 2500);
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 2500);
+}
         
- 
+ $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators:true
+
+  });
